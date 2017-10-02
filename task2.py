d3c3045bdd1ad4bae6c616c1dc1ac2d9b64f2967
abhi6292 <abhishek.rana@dal.ca>
2017-10-02 12:58:46 -0300

    task2.py

--- task2.py
+++ task2.py
@@ -1,6 +1,15 @@
-
 import csv
-with open('Crime.csv') as csvfile:
-  reader = csv.reader(csvfile)
-  for row in reader:
-     print(row[1])
+
+f = open("Crime.csv", 'rb')
+reader = csv.reader(f) #reader object which iterates over a csv file(f)
+headers = reader.next() #assign the first row to the headers variable
+column = {} #list of columns
+for h in headers: #for each header
+    column[h] = []
+for row in reader: #for each row in the reader object
+    for h, v in zip(headers, row): #combine header names with row values (v) in a series of tuples
+       column[h].append(v) 
+
+
+
+
